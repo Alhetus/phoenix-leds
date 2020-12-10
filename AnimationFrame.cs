a026b4e17cef704f69501ec995463891cedeed59
Michael Sundqvist <michael.sundqvist1@gmail.com>
2020-12-10 02:41:25 +0200

    Add AnimationFrame that will hold byte data of pixels of a single frame of animation

---
+++ AnimationFrame.cs
@@ -0,0 +1,15 @@
+﻿namespace PhoenixLeds
+{
+    public class AnimationFrame
+    {
+        public byte[] PixelBytes { get; set; }
+
+        public AnimationFrame(int frameWidth, int frameHeight)
+        {
+            // Init the array with enough capacity to hold pixel data of one frame of animation. Use 24bit pixel values.
+            // For example 12x12 pixels 24bit each (RGB channels 8 bit each)
+            // 24 / 8 = 3 bytes
+            PixelBytes = new byte[frameWidth * frameHeight * 3];
+        }
+    }
+}
