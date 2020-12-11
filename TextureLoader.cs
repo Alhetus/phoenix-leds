3a9abf5e99b23e9187750fd5059a80b9e077a903
Michael Sundqvist <michael.sundqvist1@gmail.com>
2020-12-11 02:44:08 +0200

    Add image pixel data as byte arrays to AnimationFrames, support rotated frames

--- TextureLoader.cs
+++ TextureLoader.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using PhoenixLeds.DTO;
+using System.Linq;
+using System.Runtime.InteropServices;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
@@ -10,47 +12,80 @@ namespace PhoenixLeds
 {
     public static class TextureLoader
     {
-        public static List<AnimationFrame> LoadTexture(LedAnimationDto animationDto) {
-            if (!File.Exists(animationDto.AnimationTexturePath)) {
-                Console.WriteLine($"Error: Texture '{animationDto.AnimationTexturePath}' does not exist!");
+        /// <summary>
+        /// Loads animation frames from a texture using the specified frameWidth and frameHeight.
+        /// Will resize the frames to LedGridWidth and LedGridHeight defined in global settings.
+        /// </summary>
+        public static List<AnimationFrame> LoadAnimationFramesFromTexture(int frameWidth, int frameHeight,
+            string texturePath, bool createRotatedFrames)
+        {
+            if (!File.Exists(texturePath)) {
+                Console.WriteLine($"Error: Texture '{texturePath}' does not exist!");
                 return new List<AnimationFrame>();
             }
 
-            var frameWidth = animationDto.AnimationFrameWidth;
-            var frameHeight = animationDto.AnimationFrameHeight;
-
             try {
-                using Image<Rgb24> image = Image.Load<Rgb24>(animationDto.AnimationTexturePath);
+                using Image<Rgb24> image = Image.Load<Rgb24>(texturePath);
 
                 var animationFrames = new List<AnimationFrame>();
 
                 var horizontalFrameCount = image.Width / frameWidth;
                 var verticalFrameCount = image.Height / frameHeight;
-                var animationFrameSize = GlobalSettings.LedGridSize;
+                var animationFrameWidth = GlobalSettings.LedGridWidth;
+                var animationFrameHeight = GlobalSettings.LedGridHeight;
 
                 for (var yIndex = 0; yIndex < verticalFrameCount; yIndex++) {
                     for (var xIndex = 0; xIndex < horizontalFrameCount; xIndex++) {
-                        using Image<Rgb24> copy = image.Clone();
+                        using Image<Rgb24> clone = image.Clone();
 
                         var x = xIndex * frameWidth;
                         var y = yIndex * frameHeight;
 
                         var cropRect = new Rectangle(x, y, frameWidth, frameHeight);
 
-                        copy.Mutate(img => img
+                        // Crop the image to the current frame and resize the cropped image to animation frame size
+                        clone.Mutate(img => img
                             .Crop(cropRect)
-                            .Resize(animationFrameSize, animationFrameSize, KnownResamplers.Welch));
-                        
-                        
+                            .Resize(animationFrameWidth, animationFrameHeight, KnownResamplers.Welch));
+
+                        // Get raw bytes of pixel data of the generated image
+                        var pixelBytes = GetImageBytes(clone);
+
+                        // No rotation needed, just add the frame with not-rotated byte array
+                        if (!createRotatedFrames)
+                            animationFrames.Add(new AnimationFrame(pixelBytes));
+                        // Create the rotated byte arrays
+                        else {
+                            clone.Mutate(img => img.Rotate(90));
+                            var pixelBytesRotated90 = GetImageBytes(clone);
+
+                            clone.Mutate(img => img.Rotate(90));
+                            var pixelBytesRotated180 = GetImageBytes(clone);
+
+                            clone.Mutate(img => img.Rotate(90));
+                            var pixelBytesRotated270 = GetImageBytes(clone);
+
+                            animationFrames.Add(new AnimationFrame(pixelBytes, pixelBytesRotated90,
+                                pixelBytesRotated180, pixelBytesRotated270));
+                        }
                     }
                 }
 
                 return animationFrames;
             }
-            catch (NotSupportedException e) {
-                Console.WriteLine($"Error: Could not load texture '{animationDto.AnimationTexturePath}': {e.Message}");
+            catch (Exception e) {
+                Console.WriteLine($"Error: Could not load texture '{texturePath}': {e.Message}");
                 return new List<AnimationFrame>();
             }
+        }
+
+        private static byte[] GetImageBytes(Image<Rgb24> clone) {
+            var memoryGroup = clone.GetPixelMemoryGroup();
+            var pixelData = memoryGroup
+                .SelectMany(row => MemoryMarshal.AsBytes(row.Span).ToArray())
+                .ToArray();
+
+            return pixelData;
         }
     }
 }
