369b2a7c84096b3001b70f68bc702c93a12250ff
Michael Sundqvist <michael.sundqvist1@gmail.com>
2020-12-14 01:26:39 +0200

    Add function to get animation frame bytes by panel

--- AnimationFrame.cs
+++ AnimationFrame.cs
@@ -1,4 +1,6 @@
-﻿namespace PhoenixLeds
+﻿using System;
+
+namespace PhoenixLeds
 {
     public class AnimationFrame
     {
@@ -18,6 +20,16 @@
             PixelBytesRotated90 = pixelBytesRotated90;
             PixelBytesRotated180 = pixelBytesRotated180;
             PixelBytesRotated270 = pixelBytesRotated270;
+        }
+
+        public byte[] GetBytesByPanel(Panel panel) {
+            return panel switch {
+                Panel.Left => PixelBytesRotated270 ?? new byte[0],
+                Panel.Down => PixelBytesRotated180 ?? new byte[0],
+                Panel.Up => PixelBytes,
+                Panel.Right => PixelBytesRotated90 ?? new byte[0],
+                _ => throw new ArgumentOutOfRangeException(nameof(panel), panel, null)
+            };
         }
     }
 }
