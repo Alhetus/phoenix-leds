08b8d866d3f301e3fc53248e49021f562d0a4e01
Michael Sundqvist <michael.sundqvist1@gmail.com>
2021-05-04 20:02:30 +0300

    Add ColumnIndex to AnimationEventDto

--- DTO/AnimationEventDto.cs
+++ DTO/AnimationEventDto.cs
@@ -5,16 +5,18 @@ namespace PhoenixLeds.DTO
     public class AnimationEventDto
     {
         public int PadIndex { get; }
+        public int ColumnIndex { get; }
         public string EventName { get; } = "";
 
         public AnimationEventDto(string message) {
             var parts = message.Split(';');
 
-            if (parts.Length != 2) {
-                Console.WriteLine($"Got a packet message in wrong format, must contain exactly one ';'. Message: '{message}'");
+            if (parts.Length != 3) {
+                Console.WriteLine($"Got a packet message in wrong format, must contain exactly two ';'. Message: '{message}'");
                 return;
             }
 
+            // Parse pad index
             var padIndexSuccess = int.TryParse(parts[0], out var padIndex);
 
             if (padIndexSuccess)
@@ -22,7 +24,15 @@ namespace PhoenixLeds.DTO
             else
                 Console.WriteLine($"Could not parse padIndex from message part '{parts[0]}'");
 
-            EventName = parts[1];
+            // Parse column index
+            var columnIndexSuccess = int.TryParse(parts[1], out var columnIndex);
+
+            if (columnIndexSuccess)
+                ColumnIndex = columnIndex;
+            else
+                Console.WriteLine($"Could not parse columnIndex from message part '{parts[1]}'");
+
+            EventName = parts[2].Trim();
         }
     }
 }
